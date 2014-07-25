4400d03121fa301531d6fdb3df3357df397d606c
wouthuib <wouthuib@gmail.com>
2014-07-25 18:07:40 +0200

    Update 25Jul2014
    New XNA Maplestory server

---
+++ PlayerClasses/PlayerData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace RelayServer.PlayerClasses
+{
+    [Serializable]
+    public class playerData : ISerializable
+    {
+        public string Name { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public string spritename { get; set; }
+        public string spritestate { get; set; }
+        public int prevspriteframe { get; set; }
+        public int maxspriteframe { get; set; }
+        public string attackSprite { get; set; }
+        public string spriteEffect { get; set; }
+        public string mapName { get; set; }
+        public string skincol { get; set; }
+        public string facespr { get; set; }
+        public string hairspr { get; set; }
+        public string hailcol { get; set; }
+
+        public playerData() { }
+
+        protected playerData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new System.ArgumentNullException("info");
+            Name = (string)info.GetValue("Name", typeof(string));
+            PositionX = (int)info.GetValue("PositionX", typeof(int));
+            PositionX = (int)info.GetValue("PositionY", typeof(int));
+            spritename = (string)info.GetValue("spritename", typeof(string));
+            spritestate = (string)info.GetValue("spritestate", typeof(int));
+            prevspriteframe = (int)info.GetValue("prevspriteframe", typeof(int));
+            maxspriteframe = (int)info.GetValue("maxspriteframe", typeof(int));
+            attackSprite = (string)info.GetValue("attackSprite", typeof(string));
+            spriteEffect = (string)info.GetValue("spriteEffect", typeof(string));
+            mapName = (string)info.GetValue("mapName", typeof(string));
+            skincol = (string)info.GetValue("skincol", typeof(string));
+            facespr = (string)info.GetValue("facespr", typeof(string));
+            hairspr = (string)info.GetValue("hairspr", typeof(string));
+            hailcol = (string)info.GetValue("hailcol", typeof(string));
+
+        }
+
+        [SecurityPermissionAttribute(SecurityAction.LinkDemand,
+        Flags = SecurityPermissionFlag.SerializationFormatter)]
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new System.ArgumentNullException("info");
+            info.AddValue("Name", Name);
+            info.AddValue("PositionX", PositionX);
+            info.AddValue("PositionY", PositionY);
+            info.AddValue("PositionX", PositionX);
+            info.AddValue("spritename", spritename);
+            info.AddValue("spritestate", spritestate);
+            info.AddValue("prevspriteframe", prevspriteframe);
+            info.AddValue("maxspriteframe", maxspriteframe);
+            info.AddValue("attackSprite", attackSprite);
+            info.AddValue("spriteEffect", spriteEffect);
+            info.AddValue("mapName", mapName);
+            info.AddValue("skincol", skincol);
+            info.AddValue("facespr", facespr);
+            info.AddValue("hairspr", hairspr);
+            info.AddValue("hailcol", hailcol);
+        }
+    }
+}
---
+++ PlayerClasses/PlayerStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RelayServer.PlayerClasses
+{
+    public class PlayerStore
+    {
+        private static PlayerStore instance;
+        private int maxplayers = 10, playercounter;
+        public playerData[] playerStore;
+
+        private PlayerStore()
+        {
+            playerStore = new playerData[maxplayers];
+        }
+
+        public static PlayerStore Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PlayerStore();
+                }
+                return instance;
+            }
+        }
+
+        public void addPlayer(playerData player)
+        {
+            if (playercounter < maxplayers)
+            {
+                playerStore[playercounter] = player;
+                playercounter++;
+            }
+        }
+    }
+}
